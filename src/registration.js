2e4bc047bbfa95ab82625f3f4797a648400721c7
Shad Rais <shadrais321@outlook.com>
2020-09-21 23:13:01 +0530

    Fixed issue

--- src/registration.js
+++ src/registration.js
@@ -9,26 +9,24 @@ function Registration(){
            </h1>
            </div>
            <div className='lower_part'>
-           <div>
+        
            <label className='textpos'>Name            :-</label>
            <input type='text'></input>
-           </div>
-           <div>
+        
            <label className='textpos'>Email Address   :-</label>
            <input type='text'></input>
-           </div>
-           <div>
+         
            <label className='textpos'>Address         :-</label>
            <input type='text'></input>
-           </div>
-           <div>
+           
+           
            <label className='textpos'>Phone Number    :-</label>
            <input type='text'></input>
-           </div>
-           <div>
+           
+           
            <label className='textpos'>Password         :-</label>
            <input type='text'></input>
-           </div>
+           
            </div>
            <div className='tap'>
            <label>Register</label>

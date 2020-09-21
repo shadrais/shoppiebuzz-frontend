4ad9209874146ee189cc1054127f4429a6906331
Shaan3110 <hitarasigan0987654321@gmail.com>
2020-09-21 22:56:21 +0530

    Registration

--- src/registration.js
+++ src/registration.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import './registration.css';
-
-const Registration = () => {
+function Registration(){
     return(
-       
-            <div classname='upper_part'>
-                <h1 style={{color: 'red'}}>REGISTRATION</h1>
-                <p>
-                    lorem ipsum d
-                </p>
-            </div>
-            
-       
+       <form>
+           <div>
+           <h1 className='upper_part'>
+                REGISTRATION
+           </h1>
+           </div>
+           <div className='lower_part'>
+           <div>
+           <label className='textpos'>Name            :-</label>
+           <input type='text'></input>
+           </div>
+           <div>
+           <label className='textpos'>Email Address   :-</label>
+           <input type='text'></input>
+           </div>
+           <div>
+           <label className='textpos'>Address         :-</label>
+           <input type='text'></input>
+           </div>
+           <div>
+           <label className='textpos'>Phone Number    :-</label>
+           <input type='text'></input>
+           </div>
+           <div>
+           <label className='textpos'>Password         :-</label>
+           <input type='text'></input>
+           </div>
+           </div>
+           <div className='tap'>
+           <label>Register</label>
+           </div>
+
+       </form>
     );
 };
 

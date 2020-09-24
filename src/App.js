a9080867c66942bc0eab3b06ce077b5300a86602
Shad Rais <shadrais321@outlook.com>
2020-09-24 23:34:58 +0530

    added routes front page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Frontpage from './pages/front-page/front_page'
 import Registration from './registration.js';
+
+
 function App() {
   return (
-    <div>
-      <Registration />
-    </div>
+    <Router>
+      <Switch>
+          <Route exact path="/">
+            <Frontpage />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+        </Switch>
+    </Router>
+    
+     
   );
 }
 

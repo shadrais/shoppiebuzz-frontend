9290af4bc7c4b44187b8746c80c1dc5bb798bb57
Shad Rais <shadrais321@outlook.com>
2020-11-04 10:25:10 +0530

    Added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch , Route , Redirect} from 'react-router-dom';
 import Frontpage from './pages/front-page/front_page'
-import Registration from './registration.js';
+import Loginregister from "./components/Login_register/Login_register";
 
 
 function App() {
@@ -12,9 +12,10 @@ function App() {
           <Route exact path="/">
             <Frontpage />
           </Route>
-          <Route path="/registration">
-            <Registration />
+          <Route path="/login">
+            <Loginregister />
           </Route>
+          <Redirect to={'/'} />
         </Switch>
     </Router>
     

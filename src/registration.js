eed094792867ba8e77d358d8402b608c48667020
Shaan3110 <hitarasigan0987654321@gmail.com>
2020-11-04 10:39:12 +0530

    change

---
+++ src/registration.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './registration.css';
+
+const Registration = () => {
+    return(
+       
+            <div classname='upper_part'>
+                <h1 style={{color: 'red'}}>REGISTRATION</h1>
+                <p>
+                    lorem ipsum d
+                </p>
+            </div>
+            
+       
+    );
+};
+
+export default Registration;
